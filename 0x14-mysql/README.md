78c3a68ad40e20cb486443b51521b8413b0212f6
Ben Dosch <Benjamindosch@gmail.com>
2021-04-27 17:19:43 +0000

    directory set up and files created, scirpt for sql commands work in progress

---
+++ 0x14-mysql/README.md
@@ -0,0 +1,4 @@
+# 0x14-mysql
+
+## Intro
+Directory for setting up the mysql portion of the AirB&B clone.
---
+++ 0x14-mysql/set_up.sql
@@ -0,0 +1,30 @@
+-- Script that sets up 0x14-mysql tasks
+-- Query that creates the MySQL server user holberton_user. Reminder: cat script.sql | mysql -hlocalhost -uroot -p
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost';
+-- Query that sets password for. (Identify by fails with if not exists)
+SET PASSWORD FOR 'holberton_user'@'localhost' = 'projectcorrection280hbtn';
+-- Query that grants privilages to the user.
+GRANT ALL PRIVILEGES ON *.* TO 'holberton_user'@'localhost';
+-- Shows grants for the user
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'holberton_user'@'localhost';
+-- Query that creates the MySQL server user replica_user.
+CREATE USER IF NOT EXISTS 'replica_user'@'%';
+-- Query that sets password for. (Identify by fails with if not exists)
+SET PASSWORD FOR 'replica_user'@'%' = 'replica';
+-- Query that grants privilages to the user.
+GRANT ALL PRIVILEGES ON *.* TO 'replica_user'@'%';
+-- Shows grants for the user
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'replica_user'@'%';
+-- Checks for correct permissions
+SELECT user, Repl_slave_priv FROM mysql.user;
+-- Querry to create the database tyrell_corp in your MySQL server if it does not exist.
+CREATE DATABASE IF NOT EXISTS tyrell_corp;
+-USE tyrell_corp;
+-- Querry to create a table called "nexus6" in the current database in your MySQL server.
+CREATE TABLE IF NOT EXISTS nexus6 (id INT,
+name VARCHAR(256)
+);
+-- Querry to inserts a new row in the table "nexus6" of database provided.
+INSERT INTO nexus6 (id, name) VALUES (1, "Leon");
