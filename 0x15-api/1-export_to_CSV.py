161bacbe5b6badaf17b5ed5147e450bce02fedc0
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 03:14:57 +0000

    task 1 completed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,17 @@
-#/usr/bin/python3.4
+#!/usr/bin/python3
+"""Gets API info baised on ID"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    r = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos/".
+                     format(argv[1])).json()
+    s = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                     format(argv[1])).json()
+    with open('{}.csv'.format(argv[1]), mode='w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for each in r:
+            writer.writerow([argv[1], s["username"],
+                            each["completed"], each["title"]])
