5e09e7efc685c5c57c267c741c3c16ff1a7175f5
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 04:03:21 +0000

    task 2 ready to check

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,19 @@
-#/usr/bin/python3.4
+#!/usr/bin/python3
+"""Gets API info baised on ID"""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    r = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos/".
+                     format(argv[1])).json()
+    s = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                     format(argv[1])).json()
+    with open('{}.json'.format(argv[1]), mode='w') as f:
+        data = []
+        for each in r:
+            data.append({"task": each["title"],
+                         "completed": each["completed"],
+                         "username": s["username"]})
+        json.dump({argv[1]: data}, f)
