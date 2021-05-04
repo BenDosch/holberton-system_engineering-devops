34e2b5872adf9b29a77d5b0142fd193928dc978f
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 03:03:49 +0000

    pep8 fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,15 +5,18 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    r = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos/".format(argv[1])).json()
-    s = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
+    r = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos/".
+                     format(argv[1])).json()
+    s = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                     format(argv[1])).json()
     completed = 0
     total = 0
     for each in r:
-        if each["completed"] == True:
+        if each["completed"] is True:
             completed += 1
         total += 1
-    print("Employee {} is done with tasks({}/{}):".format(s["name"], completed, total))
+    print("Employee {} is done with tasks({}/{}):".
+          format(s["name"], completed, total))
     for each in r:
-        if each["completed"] == True:
+        if each["completed"] is True:
             print("\t" + " " + each["title"])
