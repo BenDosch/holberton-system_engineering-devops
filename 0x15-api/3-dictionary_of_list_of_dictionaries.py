07ac4c7e0f322364c4e4367e2d86e6b9f4237fda
Ben Dosch <Benjamindosch@gmail.com>
2021-05-04 04:44:12 +0000

    task 3 ready to check

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,21 @@
-#/usr/bin/python3.4
+#!/usr/bin/python3
+"""Gets API info baised on ID"""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    with open('todo_all_employees.json', mode='w') as f:
+        r = requests.get("https://jsonplaceholder.typicode.com/users/").json()
+        data_dict = {}
+        for each in r:
+            s = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                             format(each["id"]) + "/todos").json()
+            data = []
+            for task in s:
+                data.append({"task": task["title"],
+                             "completed": task["completed"],
+                             "username": each["username"]})
+            data_dict[each["id"]] = data
+
+        json.dump(data_dict, f)
